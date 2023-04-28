6ccbdeeb774e509b6037eae121b4c6c70d87edd7
taiypeo <Qwertygid@gmail.com>
2023-04-28 00:02:17 +0000

    Add third-party code and draw the model

---
+++ third-party/README.md
@@ -0,0 +1,1 @@
+Helper files from https://github.com/ssloy/tinyrenderer/tree/f6fecb7ad493264ecd15e230411bfb1cca539a12
--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -3,29 +3,29 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "../third-party/model.h"
+#include "../third-party/geometry.h"
+
 const sf::Color WHITE(255, 255, 255), RED(255, 0, 0);
 
 void draw_line(sf::Image &img, int x0, int y0, int x1, int y1, const sf::Color &color)
 {
-    if (x1 < x0)
-    {
-        std::swap(x0, x1);
-    }
-    if (y1 < y0)
-    {
-        std::swap(y0, y1);
-    }
-
     bool transpose = false;
-    if (x1 - x0 < y1 - y0)
+    if (std::abs(x1 - x0) < std::abs(y1 - y0))
     {
         std::swap(x0, y0);
         std::swap(x1, y1);
         transpose = true;
     }
 
+    if (x1 < x0)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
     const int dy = y1 - y0, dx = x1 - x0;
-    int two_error = 0, y = y0;
+    int two_error = 0, y = y0, err_plus_delta = 2 * std::abs(dy), err_minus_delta = 2 * dx;
     for (int x = x0; x <= x1; ++x)
     {
         if (transpose)
@@ -37,25 +37,38 @@ void draw_line(sf::Image &img, int x0, int y0, int x1, int y1, const sf::Color &
             img.setPixel(x, y, color);
         }
 
-        two_error += 2 * dy;
+        two_error += err_plus_delta;
         if (two_error > dx)
         {
-            ++y;
-            two_error -= 2 * dx;
+            y += y1 > y0 ? 1 : -1;
+            two_error -= err_minus_delta;
         }
     }
 }
 
 int main()
 {
-    const float width = 100, height = 100;
+    const float width = 800, height = 800;
 
     sf::Image img;
     img.create(width, height, sf::Color::Black);
 
-    draw_line(img, 13, 20, 80, 40, WHITE);
-    draw_line(img, 20, 13, 40, 80, RED);
-    draw_line(img, 80, 40, 13, 20, RED);
+    Model model("obj/head.obj");
+
+    for (size_t i = 0; i < model.nfaces(); ++i)
+    {
+        const std::vector<int> face = model.face(i);
+        for (size_t j = 0; j < 3; ++j)
+        {
+            const Vec3f v0 = model.vert(face[j]), v1 = model.vert(face[(j + 1) % 3]);
+            const int x0 = (v0.x + 1.) * width / 2.,
+                      y0 = (v0.y + 1.) * height / 2.,
+                      x1 = (v1.x + 1.) * width / 2.,
+                      y1 = (v1.y + 1.) * height / 2.;
+
+            draw_line(img, x0, y0, x1, y1, WHITE);
+        }
+    }
 
     img.flipVertically();
     img.saveToFile("result.png");
