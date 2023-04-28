8d5908c7fa8e365a12d6ace3f12af35a26a83065
taiypeo <Qwertygid@gmail.com>
2023-04-28 18:09:49 +0000

    Add barycentric method

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -50,95 +50,49 @@ void draw_line(sf::Image &img, int x0, int y0, int x1, int y1, const sf::Color &
     }
 }
 
-void sort_by_y(Vec2i &p0, Vec2i &p1, Vec2i &p2)
+Vec3f barycentric_coords(
+    const Vec2i &p,
+    const Vec2i &p0,
+    const Vec2i &p1,
+    const Vec2i &p2)
 {
-    if (p0.y > p1.y)
+    const Vec2i p10 = p1 - p0,
+                p20 = p2 - p0,
+                p0p = p0 - p;
+
+    const Vec3f top_row(p10.x, p20.x, p0p.x),
+        bottom_row(p10.y, p20.y, p0p.y);
+    const Vec3f cross = top_row ^ bottom_row;
+    if (std::abs(cross.z) < 1)
     {
-        std::swap(p0, p1);
-    }
-    if (p1.y > p2.y)
-    {
-        std::swap(p1, p2);
-    }
-    if (p0.y > p1.y)
-    {
-        std::swap(p0, p1);
+        return Vec3f(-1, 1, 1);
     }
 
-    if (!(p0.y <= p1.y && p1.y <= p2.y))
-    {
-        throw std::runtime_error("Points are not sorted");
-    }
-}
-
-void fill_top_triangle(sf::Image &img, const Vec2i &top, const Vec2i &left, const Vec2i &right, const sf::Color &color)
-{
-    for (int y = top.y + 1; y <= left.y; ++y)
-    {
-        int left_x = std::round(static_cast<float>(left.x - top.x) / (left.y - top.y) * (y - top.y) + top.x);
-        int right_x = std::round(static_cast<float>(right.x - top.x) / (right.y - top.y) * (y - top.y) + top.x);
-        draw_line(img, left_x, y, right_x, y, color);
-    }
-}
-
-void fill_bottom_triangle(
-    sf::Image &img,
-    const Vec2i &bottom,
-    const Vec2i &left,
-    const Vec2i &right,
-    const sf::Color &color)
-{
-    for (int y = bottom.y - 1; y >= left.y; --y)
-    {
-        int left_x = std::round(static_cast<float>(left.x - bottom.x) / (left.y - bottom.y) * (y - bottom.y) + bottom.x);
-        int right_x = std::round(static_cast<float>(right.x - bottom.x) / (right.y - bottom.y) * (y - bottom.y) + bottom.x);
-        draw_line(img, left_x, y, right_x, y, color);
-    }
+    return Vec3f(
+        1 - (cross.x + cross.y) / cross.z, cross.x / cross.z, cross.y / cross.z);
 }
 
 void draw_triangle(sf::Image &img, Vec2i p0, Vec2i p1, Vec2i p2, const sf::Color &color)
 {
-    draw_line(img, p0.x, p0.y, p1.x, p1.y, color);
-    draw_line(img, p1.x, p1.y, p2.x, p2.y, color);
-    draw_line(img, p2.x, p2.y, p0.x, p0.y, color);
+    const int bbox_x0 = std::max(0, std::min({p0.x, p1.x, p2.x})),
+              bbox_x1 = std::min(static_cast<int>(img.getSize().x) - 1, std::max({p0.x, p1.x, p2.x})),
+              bbox_y0 = std::max(0, std::min({p0.y, p1.y, p2.y})),
+              bbox_y1 = std::min(static_cast<int>(img.getSize().y) - 1, std::max({p0.y, p1.y, p2.y}));
 
-    if (p0.y == p1.y && p1.y == p2.y || p0.x == p1.x && p1.x == p2.x)
+    for (int x = bbox_x0; x <= bbox_x1; ++x)
     {
-        return;
-    }
-
-    sort_by_y(p0, p1, p2);
-
-    if (p0.y == p1.y)
-    {
-        if (p0.x > p1.x)
+        for (int y = bbox_y0; y <= bbox_y1; ++y)
         {
-            std::swap(p0, p1);
+            const Vec2i p(x, y);
+            const Vec3f barycentric = barycentric_coords(p, p0, p1, p2);
+            if (barycentric.x < 0 || barycentric.y < 0 || barycentric.z < 0)
+            {
+                continue;
+            }
+
+            img.setPixel(x, y, color);
         }
-
-        fill_bottom_triangle(img, p2, p0, p1, color);
-        return;
     }
-    if (p1.y == p2.y)
-    {
-        if (p1.x > p2.x)
-        {
-            std::swap(p1, p2);
-        }
-
-        fill_top_triangle(img, p0, p1, p2, color);
-        return;
-    }
-
-    Vec2i division(static_cast<float>(p2.x - p0.x) / (p2.y - p0.y) * (p1.y - p0.y) + p0.x, p1.y);
-    if (division.x > p1.x)
-    {
-        std::swap(division, p1);
-    }
-
-    draw_line(img, p1.x, p1.y, division.x, division.y, color);
-    fill_top_triangle(img, p0, division, p1, color);
-    fill_bottom_triangle(img, p2, division, p1, color);
 }
 
 void cmd_draw_head()
