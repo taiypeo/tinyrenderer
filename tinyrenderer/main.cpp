ecedcc5789155af0c95d8189ef94fe1cc9a40beb
taiypeo <Qwertygid@gmail.com>
2023-04-28 15:54:02 +0000

    Add my triangle filling algorithm

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <cmath>
+#include <cstdlib>
 #include <string>
+#include <stdexcept>
 #include <iostream>
 
 #include <SFML/Graphics.hpp>
@@ -48,11 +50,95 @@ void draw_line(sf::Image &img, int x0, int y0, int x1, int y1, const sf::Color &
     }
 }
 
-void draw_triangle(sf::Image &img, const Vec2i p0, const Vec2i p1, const Vec2i p2, const sf::Color &color)
+void sort_by_y(Vec2i &p0, Vec2i &p1, Vec2i &p2)
+{
+    if (p0.y > p1.y)
+    {
+        std::swap(p0, p1);
+    }
+    if (p1.y > p2.y)
+    {
+        std::swap(p1, p2);
+    }
+    if (p0.y > p1.y)
+    {
+        std::swap(p0, p1);
+    }
+
+    if (!(p0.y <= p1.y && p1.y <= p2.y))
+    {
+        throw std::runtime_error("Points are not sorted");
+    }
+}
+
+void fill_top_triangle(sf::Image &img, const Vec2i &top, const Vec2i &left, const Vec2i &right, const sf::Color &color)
+{
+    for (int y = top.y + 1; y <= left.y; ++y)
+    {
+        int left_x = std::round(static_cast<float>(left.x - top.x) / (left.y - top.y) * (y - top.y) + top.x);
+        int right_x = std::round(static_cast<float>(right.x - top.x) / (right.y - top.y) * (y - top.y) + top.x);
+        draw_line(img, left_x, y, right_x, y, color);
+    }
+}
+
+void fill_bottom_triangle(
+    sf::Image &img,
+    const Vec2i &bottom,
+    const Vec2i &left,
+    const Vec2i &right,
+    const sf::Color &color)
+{
+    for (int y = bottom.y - 1; y >= left.y; --y)
+    {
+        int left_x = std::round(static_cast<float>(left.x - bottom.x) / (left.y - bottom.y) * (y - bottom.y) + bottom.x);
+        int right_x = std::round(static_cast<float>(right.x - bottom.x) / (right.y - bottom.y) * (y - bottom.y) + bottom.x);
+        draw_line(img, left_x, y, right_x, y, color);
+    }
+}
+
+void draw_triangle(sf::Image &img, Vec2i p0, Vec2i p1, Vec2i p2, const sf::Color &color)
 {
     draw_line(img, p0.x, p0.y, p1.x, p1.y, color);
     draw_line(img, p1.x, p1.y, p2.x, p2.y, color);
     draw_line(img, p2.x, p2.y, p0.x, p0.y, color);
+
+    if (p0.y == p1.y && p1.y == p2.y || p0.x == p1.x && p1.x == p2.x)
+    {
+        return;
+    }
+
+    sort_by_y(p0, p1, p2);
+
+    if (p0.y == p1.y)
+    {
+        if (p0.x > p1.x)
+        {
+            std::swap(p0, p1);
+        }
+
+        fill_bottom_triangle(img, p2, p0, p1, color);
+        return;
+    }
+    if (p1.y == p2.y)
+    {
+        if (p1.x > p2.x)
+        {
+            std::swap(p1, p2);
+        }
+
+        fill_top_triangle(img, p0, p1, p2, color);
+        return;
+    }
+
+    Vec2i division(static_cast<float>(p2.x - p0.x) / (p2.y - p0.y) * (p1.y - p0.y) + p0.x, p1.y);
+    if (division.x > p1.x)
+    {
+        std::swap(division, p1);
+    }
+
+    draw_line(img, p1.x, p1.y, division.x, division.y, color);
+    fill_top_triangle(img, p0, division, p1, color);
+    fill_bottom_triangle(img, p2, division, p1, color);
 }
 
 void cmd_draw_head()
@@ -77,6 +163,39 @@ void cmd_draw_head()
 
             draw_line(img, x0, y0, x1, y1, WHITE);
         }
+    }
+
+    img.flipVertically();
+    img.saveToFile("result.png");
+}
+
+void cmd_draw_filled_head()
+{
+    const float width = 800, height = 800;
+
+    sf::Image img;
+    img.create(width, height, sf::Color::Black);
+
+    Model model("obj/head.obj");
+
+    for (size_t i = 0; i < model.nfaces(); ++i)
+    {
+        const std::vector<int> face = model.face(i);
+        Vec2i screen_coords[3];
+        for (size_t j = 0; j < 3; ++j)
+        {
+            const Vec3f world_coords = model.vert(face[j]);
+            screen_coords[j] = Vec2i(
+                (world_coords.x + 1.) * width / 2.,
+                (world_coords.y + 1.) * height / 2.);
+        }
+
+        draw_triangle(
+            img,
+            screen_coords[0],
+            screen_coords[1],
+            screen_coords[2],
+            sf::Color(rand() % 255, rand() % 255, rand() % 255));
     }
 
     img.flipVertically();
@@ -114,6 +233,10 @@ int main(int argc, char *argv[])
     if (command == "head")
     {
         cmd_draw_head();
+    }
+    else if (command == "filled-head")
+    {
+        cmd_draw_filled_head();
     }
     else if (command == "triangle")
     {
