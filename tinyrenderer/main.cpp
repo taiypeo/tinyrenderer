f67f485bce4a6253f6eee0538996a201f6edf125
taiypeo <Qwertygid@gmail.com>
2023-04-28 00:18:51 +0000

    Add commands

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,12 +1,14 @@
 #include <algorithm>
 #include <cmath>
+#include <string>
+#include <iostream>
 
 #include <SFML/Graphics.hpp>
 
 #include "../third-party/model.h"
 #include "../third-party/geometry.h"
 
-const sf::Color WHITE(255, 255, 255), RED(255, 0, 0);
+const sf::Color WHITE(255, 255, 255), RED(255, 0, 0), GREEN(0, 255, 0);
 
 void draw_line(sf::Image &img, int x0, int y0, int x1, int y1, const sf::Color &color)
 {
@@ -46,7 +48,14 @@ void draw_line(sf::Image &img, int x0, int y0, int x1, int y1, const sf::Color &
     }
 }
 
-int main()
+void draw_triangle(sf::Image &img, const Vec2i p0, const Vec2i p1, const Vec2i p2, const sf::Color &color)
+{
+    draw_line(img, p0.x, p0.y, p1.x, p1.y, color);
+    draw_line(img, p1.x, p1.y, p2.x, p2.y, color);
+    draw_line(img, p2.x, p2.y, p0.x, p0.y, color);
+}
+
+void cmd_draw_head()
 {
     const float width = 800, height = 800;
 
@@ -72,6 +81,49 @@ int main()
 
     img.flipVertically();
     img.saveToFile("result.png");
+}
+
+void cmd_draw_triangle()
+{
+    const float width = 200, height = 200;
+
+    sf::Image img;
+    img.create(width, height, sf::Color::Black);
+
+    Vec2i p0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+    Vec2i p1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+    Vec2i p2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    draw_triangle(img, p0[0], p0[1], p0[2], RED);
+    draw_triangle(img, p1[0], p1[1], p1[2], WHITE);
+    draw_triangle(img, p2[0], p2[1], p2[2], GREEN);
+
+    img.flipVertically();
+    img.saveToFile("result.png");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./tinyrenderer COMMAND" << std::endl;
+        return 1;
+    }
+
+    const std::string command(argv[1]);
+    if (command == "head")
+    {
+        cmd_draw_head();
+    }
+    else if (command == "triangle")
+    {
+        cmd_draw_triangle();
+    }
+    else
+    {
+        std::cerr << "Unknown command" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
